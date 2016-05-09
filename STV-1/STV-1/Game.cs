bf7430f10bd4ab63afa84453715edaf6ae6c6a84
PyotrRomanov <bor.greve@gmail.com>
2016-05-09 22:43:35 +0200

    basis classes aangemaakt

---
+++ STV-1/STV-1/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Game
+{
+    Game() {
+        Dungeon curDungeon = new Dungeon();
+    }
+
+    void Save() {
+        // TODO
+    }
+
+    void Load() {
+        // TODO
+    }
+
+    void NextDungeon() {
+        // TODO
+    }
+
+
+}
