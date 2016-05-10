8f305a7dea06ec1b196f714dbbf2e2e6ccf6ce1a
Petar Kostic <snookik@gmail.com>
2016-05-10 17:30:53 +0200

    merge met classes en details van Bor

--- STV-1/STV-1/Node.cs
+++ STV-1/STV-1/Node.cs
@@ -10,15 +10,18 @@ class Node
     private List<Node> Connections;
     private int level;
     private int maxmonsters;
-       
+    public string type;
+    public List<Item> loot;
+
     /// <summary>
     /// Creates a new node that can be occupied by a player, monster packs or both.
     /// There's a limit of how many monsters each node u can accomodate
     /// </summary>
     /// <param name="level">Node u's level</param>
     /// <param name="M">constant which is the same over the whole dungeon</param>
-    public Node(int level, int M)
+    public Node(int level, int M, string type = "normal")
     {
+        this.type = type;
         this.level = level;
         this.maxmonsters = M*(level + 1);
         Connections = new List<Node>();
