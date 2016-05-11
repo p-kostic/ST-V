f0b79073dd63bfc87e4aac2a17a32485b2c529c2
Markieautarkie <markberentsen@hotmail.com>
2016-05-11 15:11:29 +0200

    Pack zo goed als af, nog even commenten

--- STV-1/STV-1/Creatures/Pack.cs
+++ STV-1/STV-1/Creatures/Pack.cs
@@ -4,20 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * In this class we will define the packs. Monsters will be grouped
+ * In packs, and in this class we will handle a few methods like
+ * movement, attacking and creating the actual pack.
+ */
 class Pack
 {
-    List<Creature> monsters;
-    Node curPos;
+    List<Monster> monsters;
 
-    Pack(int number) {
-        
+    public Pack(int packSize, Node packStartLocation)
+    {
+        monsters = new List<Monster>();
+        for (int i = 0; i < packSize; i++)
+        {
+            Monster monster = new Monster(10, 2, packStartLocation);
+            monsters.Add(monster);
+        }
     }
 
-    void Move() {
-        // TODO
+    public void MovePack(Node destination)
+    {
+        foreach(Monster monster in monsters)
+        {
+            monster.Location = destination;
+        }
     }
 
-    void Attack() {
-        // TODO
+    public void PackAttack(Creature creature)
+    {
+        foreach(Monster monster in monsters)
+        {
+            monster.Attack(creature);
+        }
+    }
+
+    public int PackSize { get { return monsters.Count; } }
+    public Node PackLocation { get { return monsters[0].Location; } }
+    public bool PackDied
+    {
+        get
+        {
+            if (monsters.Count == 0)
+                return true;
+            else
+                return false;
+        }
     }
 }
