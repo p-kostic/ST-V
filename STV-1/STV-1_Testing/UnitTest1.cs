a8b4fe65098e5a900b4af14cf0d6a5d1841fba11
Markieautarkie <markberentsen@hotmail.com>
2016-05-11 20:21:22 +0200

    Unit testing project toegevoegd

---
+++ STV-1/STV-1_Testing/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace STV_1_Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
