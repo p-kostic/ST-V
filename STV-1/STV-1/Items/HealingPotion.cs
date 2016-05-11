cf503b68584972d7996f275193f60abcdaa967d7
Markieautarkie <markberentsen@hotmail.com>
2016-05-11 12:17:00 +0200

    Healingpotion wat uitgewerkt

--- STV-1/STV-1/Items/HealingPotion.cs
+++ STV-1/STV-1/Items/HealingPotion.cs
@@ -6,5 +6,25 @@ using System.Threading.Tasks;
 
 class HealingPotion : Item
 {
-    public int healing;
+    int healValue = 0;
+    bool used;
+    
+
+    public HealingPotion()
+    {
+        Random random = new Random();
+        healValue = random.Next(1, 20);
+    }
+
+    public void destroy()
+    {
+        if (used)
+        {
+            // Destroy potion
+        }
+    }
+
+    // Getters and setters for the variables.
+    public int HealValue { get { return healValue; } }
+    public bool Used { set { used = value; } }
 }
